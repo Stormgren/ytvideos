4035b14fce4ceca910a3477f27064f71bc3618a4
Slobodan Vukovic <sbnv93@gmail.com>
2019-12-26 19:26:50 +0100

    Fixed minor bugs and errors and adjusted appearance of the app

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,10 +3,17 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video) => {
-        return <VideoItem onVideoSelect={onVideoSelect} video={video}/>
+        return (
+        <VideoItem 
+        key={video.etag} 
+        onVideoSelect={onVideoSelect} 
+        video={video}
+        />)
     });
 
-    return <div className="ui relaxed divided list">{renderedList}</div>
-}
+    return (
+    <div className="ui relaxed divided list">{renderedList}</div>
+    )
+};
 
 export default VideoList;
